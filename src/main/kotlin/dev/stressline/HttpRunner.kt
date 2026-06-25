8844f6ab8001c8e9190361b732f168fa3d2ac690
Gergo Kallai <gerg.kallai@gmail.com>
2026-06-25 21:06:11 +0200

    feat: add Ktor HTTP runner

---
+++ src/main/kotlin/dev/stressline/HttpRunner.kt
@@ -0,0 +1,45 @@
+package dev.stressline
+
+import io.ktor.client.HttpClient
+import io.ktor.client.request.header
+import io.ktor.client.request.request
+import io.ktor.client.request.setBody
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpMethod
+import kotlinx.coroutines.CancellationException
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration
+import kotlin.time.measureTimedValue
+
+interface HttpRunner {
+    suspend fun execute(): RequestResult
+}
+
+class KtorHttpRunner(
+    private val client: HttpClient,
+    private val config: RunConfig,
+) : HttpRunner {
+    override suspend fun execute(): RequestResult {
+        return try {
+            val timed = measureTimedValue {
+                withTimeout(config.timeout) {
+                    val response = client.request(config.url) {
+                        method = HttpMethod.parse(config.method)
+                        config.headers.forEach { (k, v) -> header(k, v) }
+                        config.body?.let { setBody(it) }
+                    }
+                    response.bodyAsText() // drain so the connection is released
+                    response.status.value
+                }
+            }
+            RequestResult(timed.duration, RequestError.fromStatus(timed.value))
+        } catch (e: TimeoutCancellationException) {
+            RequestResult(config.timeout, RequestError.Timeout)
+        } catch (e: CancellationException) {
+            throw e
+        } catch (e: Throwable) {
+            RequestResult(Duration.ZERO, RequestError.fromException(e))
+        }
+    }
+}
